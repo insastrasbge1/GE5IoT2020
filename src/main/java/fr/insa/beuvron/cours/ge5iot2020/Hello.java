bfbd3cf780a81116b8de9ae2e664df27d7a8ffc1
francois <francois@FDBMAISON>
2020-11-01 15:38:27 +0100

    test github2

--- src/main/java/fr/insa/beuvron/cours/ge5iot2020/Hello.java
+++ src/main/java/fr/insa/beuvron/cours/ge5iot2020/Hello.java
@@ -12,7 +12,7 @@ package fr.insa.beuvron.cours.ge5iot2020;
 public class Hello {
     
     public static void main(String[] args) {
-        System.out.println("Hello !!");
+        System.out.println("Hello 2 !!");
     }
     
 }
